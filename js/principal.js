b9e1482ff679e26649353133ff25d6ed49dc0a7f
Mateus Ramos Pereira <mateus.pereira@docente.pr.senac.br>
2025-02-04 12:29:04 -0300

    Cálculo de IMC

---
+++ js/principal.js
@@ -0,0 +1,17 @@
+// Alteração do título da página
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+// Cálculo do IMC do paciente
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var tdAltura = paciente.querySelector(".info-altura");
+
+var peso = tdPeso.textContent;
+var altura = tdAltura.textContent;
+
+var imc = peso / (altura * altura);
+
+var tdImc = paciente.querySelector(".info-imc");
+tdImc.textContent = imc.toFixed(2);
