cf778665fbea8ac04d141de1489dde40dd04660a
Mateus Ramos Pereira <mateus.pereira@docente.pr.senac.br>
2025-02-05 16:32:17 -0300

    Cálculo de IMC dos pacientes da tabela

--- js/principal.js
+++ js/principal.js
@@ -3,32 +3,35 @@ var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
 // Cálculo do IMC do paciente
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdPeso = paciente.querySelector(".info-peso");
-var tdAltura = paciente.querySelector(".info-altura");
+for (var i = 0; i < pacientes.length; i++) {
+    var paciente = pacientes[i];
 
-var tdImc = paciente.querySelector(".info-imc");
+    var tdPeso = paciente.querySelector(".info-peso");
+    var tdAltura = paciente.querySelector(".info-altura");
 
-var alturaEhValida = true;
-var pesoEhValido = true;
+    var tdImc = paciente.querySelector(".info-imc");
 
-var peso = tdPeso.textContent;
-if (peso <= 0 || peso > 600) {
-    console.log("Peso inválido!");
-    pesoEhValido = false;
+    var alturaEhValida = true;
+    var pesoEhValido = true;
+
+    var peso = tdPeso.textContent;
+    if (peso <= 0 || peso > 600) {
+        console.log("Peso inválido!");
+        pesoEhValido = false;
+    }
+
+    var altura = tdAltura.textContent;
+    if (altura <= 0 || altura > 3) {
+        console.log("Altura inválida!");
+        alturaEhValida = false;
+    }
+
+    if (alturaEhValida && pesoEhValido) {
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    } else {
+        tdImc.textContent = "Altura e/ou peso inválidos!"
+    }
 }
-
-var altura = tdAltura.textContent;
-if (altura <= 0 || altura > 3) {
-    console.log("Altura inválida!");
-    alturaEhValida = false;
-}
-
-if (alturaEhValida && pesoEhValido) {
-    var imc = peso / (altura * altura);
-    tdImc.textContent = imc.toFixed(2);
-} else {
-    tdImc.textContent = "Altura e/ou peso inválidos!"
-}
-
