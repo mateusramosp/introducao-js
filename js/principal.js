6d8cc54723faaac42e1e536985f9c981de668313
Mateus Ramos Pereira <mateus.pereira@docente.pr.senac.br>
2025-02-05 12:23:06 -0300

    Validação de peso e altura

--- js/principal.js
+++ js/principal.js
@@ -8,10 +8,27 @@ var paciente = document.querySelector("#primeiro-paciente");
 var tdPeso = paciente.querySelector(".info-peso");
 var tdAltura = paciente.querySelector(".info-altura");
 
-var peso = tdPeso.textContent;
-var altura = tdAltura.textContent;
-
-var imc = peso / (altura * altura);
-
 var tdImc = paciente.querySelector(".info-imc");
-tdImc.textContent = imc.toFixed(2);
+
+var alturaEhValida = true;
+var pesoEhValido = true;
+
+var peso = tdPeso.textContent;
+if (peso <= 0 || peso > 600) {
+    console.log("Peso inválido!");
+    pesoEhValido = false;
+}
+
+var altura = tdAltura.textContent;
+if (altura <= 0 || altura > 3) {
+    console.log("Altura inválida!");
+    alturaEhValida = false;
+}
+
+if (alturaEhValida && pesoEhValido) {
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc.toFixed(2);
+} else {
+    tdImc.textContent = "Altura e/ou peso inválidos!"
+}
+
